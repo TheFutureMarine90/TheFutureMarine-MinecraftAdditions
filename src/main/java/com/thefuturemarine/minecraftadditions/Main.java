9eb8462a3798054d7e2e32428dca81008820aeea
TheFutureMarine90 <thefuturemarine99@gmail.com>
2017-12-22 18:11:01 -0500

    Added Reference file

---
+++ src/main/java/com/thefuturemarine/minecraftadditions/Main.java
@@ -0,0 +1,9 @@
+package com.thefuturemarine.minecraftadditions;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod()
+public class Main
+{
+
+}
---
+++ src/main/java/com/thefuturemarine/minecraftadditions/Reference.java
@@ -0,0 +1,13 @@
+package com.thefuturemarine.minecraftadditions;
+
+public class Reference
+{
+    public static final String MODID = "minecraftadditions";
+    public static final String NAME = "Minecraft Additions";
+    public static final String VERSION = "1.0";
+    public static final String MCVERSION = "1.12.2";
+
+    public static final String CLIENT = "com.thefuturemarine.minecraftadditions.proxy.ClientProxy";
+    public static final String SERVER = "com.thefuturemarine.minecraftadditions.proxy.CommonProxy";
+
+}
